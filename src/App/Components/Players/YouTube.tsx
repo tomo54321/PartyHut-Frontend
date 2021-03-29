c4b6f109cd22713b646bb6d840199fa7605c75ed
Tom <tryhardgaminguk@googlemail.com>
2021-03-29 12:18:43 +0100

    Ref TypeSafe

--- src/App/Components/Players/YouTube.tsx
+++ src/App/Components/Players/YouTube.tsx
@@ -20,7 +20,7 @@ export const YTPlayer: React.FC<YouTubePlayerProps> = ({
     volume,
     playerRef
 }) => {
-    const player = useRef(null as null | YouTubePlayer);
+    const player = useRef<YouTubePlayer>(null);
     return (
         <div className="aspect-h-9 aspect-w-16">
             <ReactPlayer
