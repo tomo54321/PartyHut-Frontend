1092eb668aff6bf3931d6754a8ac5181f3f78436
TomR2018 <thomas.reeve@richardsonsgroup.net>
2021-04-02 09:19:15 +0100

    Song UI Fix

--- src/App/Components/SongRow.tsx
+++ src/App/Components/SongRow.tsx
@@ -17,7 +17,7 @@ export const SongRow: React.FC<SongRowProps> = ({
     Button
 }) => (
     <div className="flex flex-wrap sm:flex-nowrap md:space-x-3 pb-3 sm:pb-0 border-b border-gray-700 sm:border-b-0 items-center">
-        <img src={thumbnailUrl} alt={title} className="hidden md:block flex-shrink-0 w-24 h-24" />
+        <img src={thumbnailUrl} alt={title} className="hidden md:block flex-shrink-0 w-24 h-24 rounded-md" />
         <div className="space-y-1 w-full">
             <span className="block truncate md:max-w-xs lg:max-w-md xl:max-w-lg 2xl:max-w-none font-medium text-lg">{title}</span>
             <span className="block truncate md:max-w-xs lg:max-w-md xl:max-w-lg 2xl:max-w-none opacity-75">{postedBy}</span>
